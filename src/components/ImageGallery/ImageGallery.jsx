3d960434f6fa926ef7a478912feb3aea2193b307
YVakhnyak <namejfk@gmail.com>
2023-03-03 12:46:15 +0200

    fixed little bugs

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -9,15 +9,12 @@ import { Notify } from 'notiflix';
 
 const API_KEY = '33411326-e3b74484d09501fb125cb8795';
 let perPage = 12;
-console.log('Image gallery is here!');
 
 export default function ImageGallery({ searchValue }) {
   const [images, setImages] = useState(null);
   const [status, setStatus] = useState('idle');
   const [error, setError] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
-
-  console.log(` Gallery searchValue: ${searchValue}`);
 
   const loadMoreImages = () => {
     setPageNumber(prevPageNumber => prevPageNumber + 1);
@@ -31,10 +28,10 @@ export default function ImageGallery({ searchValue }) {
     setStatus('pending');
     setPageNumber(1);
 
-    let inputQuery = searchValue;
+    let inputQuery = searchValue.trim();
     console.log(`inputQuery: ${searchValue}`);
 
-    // .trim();
+    //
 
     const searchParams = new URLSearchParams({
       key: API_KEY,
@@ -50,7 +47,6 @@ export default function ImageGallery({ searchValue }) {
 
     fetch(`https://pixabay.com/api/?${searchParams}`)
       .then(response => {
-        console.log('response is almost here!');
         if (response.ok) {
           return response.json();
         }
@@ -69,7 +65,7 @@ export default function ImageGallery({ searchValue }) {
   if (status === 'idle') {
     return (
       <div className={css.firstSpinner}>
-        Тут будуть картинки за Вашим запитом...
+        Here will be pictures at your request...
       </div>
     );
   }
@@ -85,7 +81,7 @@ export default function ImageGallery({ searchValue }) {
     return <ImagesErrorView message={error.message} />;
   }
   if (status === 'resolved' && images.totalHits === 0) {
-    return Notify.failure('На жаль, нічого не знайдено...');
+    return Notify.failure('Unfortunately, nothing was found...');
   }
   if (status === 'resolved') {
     const startIndex = 0;
